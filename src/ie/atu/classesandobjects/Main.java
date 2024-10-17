d633e1361e79a84af445a07c6290112cc4cab753
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-10-17 15:13:43 +0000

    Update Main class comments for clarity on Student object creation and method calls

--- src/ie/atu/classesandobjects/Main.java
+++ src/ie/atu/classesandobjects/Main.java
@@ -2,12 +2,12 @@ package ie.atu.classesandobjects;
 
 public class Main {
    public static void main(String[] args) {
-      // Create an object of the Student class
+      // Create an object of the Student class for each constructor
       Student studentObject1 = new Student();
       Student studentObject2 = new Student("S00123");
       Student studentObject3 = new Student("S00456", 20, true);
 
-      // Call the displayInfo method on the studentObject1
+      // Call the displayInfo method on each object
       studentObject1.displayInfo();
       studentObject2.displayInfo();
       studentObject3.displayInfo();
@@ -15,10 +15,10 @@ public class Main {
       // Call the static method getStudentCount
       System.out.println("Student Count: " + Student.getStudentCount());
 
-      // Call the toString method
+      // Call the toString method on each object
       System.out.println(studentObject1);
       System.out.println(studentObject2);
       System.out.println(studentObject3);
 
-   }
-}
+   } // End of main method
+} // End of Main class
