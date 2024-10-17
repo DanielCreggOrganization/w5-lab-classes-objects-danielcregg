303ad9cabd39c09cec50dc34ae1fdf4669eb4f82
Daniel Cregg <danielcregg@users.noreply.github.com>
2024-10-17 14:40:52 +0000

    Add constructor to Student class and update Main to use it

--- src/ie/atu/classesandobjects/Main.java
+++ src/ie/atu/classesandobjects/Main.java
@@ -3,7 +3,7 @@ package ie.atu.classesandobjects;
 public class Main {
    public static void main(String[] args) {
       // Create an object of the Student class
-      Student studentObject1 = new Student();
+      Student studentObject1 = new Student("S00123", 20, true);
 
       // Call the displayInfo method on the studentObject1
       studentObject1.displayInfo();
--- src/ie/atu/classesandobjects/Student.java
+++ src/ie/atu/classesandobjects/Student.java
@@ -6,6 +6,13 @@ public class Student {
     int age;
     boolean isRegistered;
 
+    // Constructor
+    public Student(String studentID, int age, boolean isRegistered) {
+        this.studentID = studentID;
+        this.age = age;
+        this.isRegistered = isRegistered;
+    }
+
     // Method to display the student information
     public void displayInfo() {
         System.out.println("Student ID: " + studentID);
